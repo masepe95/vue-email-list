2737262ff6f7110dcaf37a919157864c9154f0bb
masepe95 <marcosepe10@gmail.com>
2023-06-27 18:08:09 +0200

    [HTML] and [JS SCRIPT] added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 console.log('JS OK', Vue) 
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+
+const { createApp } = Vue;
+ 
+const app = createApp({
+    data() {
+      return {
+        emails: [],
+        items: 10,
+        isLoading: false
+      }
+    },
+
+    mounted() {
+        this.isLoading = true;
+      for(let i = 0; i < this.items; i++){
+        axios.get(endpoint).then(res => {
+          const newMail = res.data.response
+          this.emails.push(newMail)
+        })
+        this.isLoading = false;
+      }
+    }
+  })
+  app.mount("#root");
