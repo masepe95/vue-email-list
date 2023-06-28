cafa5b3811fe3ea3cdeeabfae87ad5182bc78e1c
masepe95 <marcosepe10@gmail.com>
2023-06-28 09:27:11 +0200

    isLoading fix

--- js/script.js
+++ js/script.js
@@ -10,18 +10,22 @@ const app = createApp({
       return {
         emails: [],
         items: 10,
-        isLoading: false
+      }
+    },
+    computed: {
+      isLoading(){
+        return this.emails.length !== this.items;
       }
     },
 
     mounted() {
-        this.isLoading = true;
+        
       for(let i = 0; i < this.items; i++){
         axios.get(endpoint).then(res => {
           const newMail = res.data.response
           this.emails.push(newMail)
         })
-        this.isLoading = false;
+        
       }
     }
   })
